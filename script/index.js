4870931281ffa5b2fe929b83047ac9a38fec020b
Ivaylo Kolev <ivaylo.kolev.fgd@gmail.com>
2024-03-15 00:05:13 +0000

    added some styling

--- script/index.js
+++ script/index.js
@@ -1,28 +1,36 @@
-const cron = require('node-cron');
+async function fetchRafflesAndUpdateList() {
+    try {
+        // Fetch data from the server to update the list of active raffles
+        const response = await fetch('/all-raffles');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
 
-cron.schedule('*/1 * * * * *', async () => {
+        const raffleList = document.getElementById('active-raffles-list');
+        raffleList.innerHTML = '';
 
-    // Fetch data from the server every minute to keep up to date
-    fetch('/all-raffles')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(raffles => {
-            // Filter out raffles with end dates in the past
-            const activeRaffles = raffles.filter(raffle => new Date(raffle.endDate) > new Date());
+        // Filter out raffles with end dates in the past
+        const activeRaffles = data.filter(raffle => new Date(raffle.endDate) > new Date());
 
-            // Populate the list with active raffles
-            const raffleList = document.getElementById('active-raffles-list');
-            activeRaffles.forEach(raffle => {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${raffle.name}: ${raffle.prize}`;
-                raffleList.appendChild(listItem);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching or processing raffles:', error);
+        activeRaffles.forEach(raffle => {
+            const listItem = document.createElement('li');
+            let participants = raffle.participants.length > 0 ? raffle.participants.join(', ') : 'None';
+            listItem.innerHTML = `Name: ${raffle.name},<br>
+                              Start Date: ${new Date(raffle.startDate).toLocaleString()},<br>
+                              End Date: ${new Date(raffle.endDate).toLocaleString()},<br>
+                              Draw Date: ${new Date(raffle.drawDate).toLocaleString()},<br>
+                              Prize: ${raffle.prize},<br>
+                              Participants: ${participants}`; // Assuming participants is an array
+            raffleList.appendChild(listItem);
         });
-});
+    } catch (error) {
+        console.error('Error fetching or processing raffles:', error);
+    }
+}
+
+// Call fetchRafflesAndUpdateList initially
+fetchRafflesAndUpdateList();
+
+// Schedule periodic updates every thirty seconds
+setInterval(fetchRafflesAndUpdateList, 30000);
